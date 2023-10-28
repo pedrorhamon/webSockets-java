3a87515fb1129aa11925051448cc300de067a732
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-10-28 19:42:37 -0300

    Criando class webSockets

---
+++ src/main/java/com/starking/websockets/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.starking.websockets;
+
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * @author pedroRhamon
+ */
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		// TODO Auto-generated method stub
+		WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+	}
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		// TODO Auto-generated method stub
+		WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+	}
+	
+	
+
+}
