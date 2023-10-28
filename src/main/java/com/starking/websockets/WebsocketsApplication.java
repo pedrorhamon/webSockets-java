ea199c2a8e03fd790aeebb2b36cd6326b2addd64
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-10-28 19:31:47 -0300

    removendo alguns partes do spring maven

--- src/main/java/com/starking/websockets/WebsocketsApplication.java
+++ src/main/java/com/starking/websockets/WebsocketsApplication.java
@@ -1,13 +1,9 @@
 package com.starking.websockets;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class WebsocketsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebsocketsApplication.class, args);
 	}
 
 }
--- src/test/java/com/starking/websockets/WebsocketsApplicationTests.java
+++ src/test/java/com/starking/websockets/WebsocketsApplicationTests.java
@@ -1,12 +1,8 @@
 package com.starking.websockets;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class WebsocketsApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
