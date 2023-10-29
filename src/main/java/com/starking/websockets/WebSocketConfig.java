342d01c2cefb2cf2982075e807a80c8dd8f04096
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-10-29 12:59:34 -0300

    adicionando os arquivos no diretorio correto

--- src/main/java/com/starking/websockets/WebSocketConfig.java
+++ src/main/java/com/starking/websockets/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.starking.websockets;
+	package com.starking.websockets;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
